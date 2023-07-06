35a60b296e07de3202c4108f16bf7da05b2c31b1
AE/BHH <ehsaniabbas9@gmail.com>
2023-07-06 08:02:13 -0600

    code refactor in js was successfully made

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ $(document).ready(function () {
 	const searchBtn = $('#search-btn')
 	const listContainer = $('#search-list-container')
 	const bookFilterDropdown = $('#filter-dropdown')
-    const main = $('#main')
+	const spinner = $('#spinner')
+	const main = $('#main')
+	hideSpinner()
 
 	handleSearch()
 	function handleSearch() {
 		searchBtn.on('click', (e) => {
 			e.preventDefault()
-            main.hide()
-            searchBoxInput.empty();
-			console.log('works!')
+			main.hide()
+			searchBoxInput.empty()
 			const searchInput = searchBoxInput.val()
 			const selectedFilter = bookFilterDropdown.val()
 			if (searchInput === '') {
@@ -19,44 +20,64 @@ $(document).ready(function () {
 			} else {
 				const searchURL = `https://openlibrary.org/search.json?${selectedFilter}=${searchInput}`
 
+				showSpinner()
+
 				fetch(searchURL)
 					.then((res) => res.json())
 					.then((data) => {
-						data.docs.forEach((element) => {
+						hideSpinner()
 
-                            const bookContainer = $('<div></div>').attr('class', 'row bookContainer')
-                            
-							getBookCover()
-							function getBookCover() {
-								let coverId = element.cover_i
-                                const coverPhoto = $('<div></div>').attr('class', 'col-4 bookImage')
-								coverPhoto.append(
-									`<span class="imageContainer"><a class="bookLink" href="https://openlibrary.org${element.key}"><img width="100" height="150" margin= "50px" src="https://covers.openlibrary.org/b/id/${coverId}.jpg" alt="Cover not found"/></a></span>`
+						if (data.docs.length === 0) {
+							alert('No result found for your search!')
+							hideSpinner()
+						} else {
+							data.docs.forEach((element) => {
+								const bookContainer = $('<div></div>').attr(
+									'class',
+									'row bookContainer'
 								)
-								bookContainer.append(coverPhoto)
-							}
+								getBookCover(element, bookContainer)
+								getBookInfo(element, bookContainer)
 
-							getBookInfo()
-							function getBookInfo() {
-								let bookAuthor = `Author: ${element.author_name}`
-								let language = `Language(s): ${element.language}`
-								let pages = `Pages: ${element.number_of_pages_median}`
-								let firstPublished = `First Published Year: ${element.first_publish_year}`
-
-                                const bookInfo = $('<div></div>').attr('class', 'col bookInfo')
-
-
-								bookInfo.append(
-									`<span class="bookInfoContainer"><a href="https://openlibrary.org${element.key}">${element.title}</a> <br/> ${bookAuthor} <br/> ${language} <br/> ${pages} <br/> ${firstPublished}</span>`
-								)
-                                bookContainer.append(bookInfo)
-								
-							}
-							listContainer.append(bookContainer)
-						})
+								listContainer.append(bookContainer)
+							})
+						}
 					})
-					.catch((err) => console.error(err.message))
+					.catch((err) => {
+						hideSpinner()
+						console.error(err.message)
+					})
 			}
 		})
+	}
+
+	function getBookInfo(element, bookContainer) {
+		let bookAuthor = `Author: ${element.author_name}`
+		let language = `Language(s): ${element.language}`
+		let pages = `Pages: ${element.number_of_pages_median}`
+		let firstPublished = `First Published Year: ${element.first_publish_year}`
+
+		const bookInfo = $('<div></div>').attr('class', 'col bookInfo')
+		bookInfo.append(
+			`<span class="bookInfoContainer"><a href="https://openlibrary.org${element.key}">${element.title}</a> <br/> ${bookAuthor} <br/> ${language} <br/> ${pages} <br/> ${firstPublished}</span>`
+		)
+		bookContainer.append(bookInfo)
+	}
+
+	function getBookCover(element, bookContainer) {
+		let coverId = element.cover_i
+		const coverPhoto = $('<div></div>').attr('class', 'col-4 bookImage')
+		coverPhoto.append(
+			`<span class="imageContainer"><a class="bookLink" href="https://openlibrary.org${element.key}"><img width="100" height="150" margin= "50px" src="https://covers.openlibrary.org/b/id/${coverId}.jpg" alt="Cover not found"/></a></span>`
+		)
+		bookContainer.append(coverPhoto)
+	}
+
+	function showSpinner() {
+		spinner.show()
+	}
+
+	function hideSpinner() {
+		spinner.hide()
 	}
 })
